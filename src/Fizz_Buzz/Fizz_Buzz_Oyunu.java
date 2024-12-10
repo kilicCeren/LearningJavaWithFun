6235392f4095ad5c4bd8120a8568ed606eca8f60
kilicCeren <kiliccrn07@gmail.com>
2024-12-10 23:54:17 +0300

    v1

--- src/Fizz_Buzz/Fizz_Buzz_Oyunu.java
+++ src/Fizz_Buzz/Fizz_Buzz_Oyunu.java
@@ -16,10 +16,10 @@ public class Fizz_Buzz_Oyunu {
         System.out.println("Lutfen pozitif bir tamsayi giriniz:");
         int sayi = scan.nextInt();
 
-        for (int i = 1; i <=sayi ; i++) {
+        for (int i = 1; i <= sayi; i++) {
             if (i % 5 == 0 && i % 3 == 0) System.out.println("fizzBuzz");
-            else if (i % 5==0) System.out.print("buzz" + " ");
-            else if (i % 3==0) System.out.print("fizz" + " ");
+            else if (i % 5 == 0) System.out.print("buzz" + " ");
+            else if (i % 3 == 0) System.out.print("fizz" + " ");
             else System.out.print(i + " ");
         }
     }
